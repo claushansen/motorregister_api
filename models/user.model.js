bdee1bc0adc340226ed781257f2b57a5669768fb
Claus Hansen <claus@multimedion.dk>
2016-03-22 08:36:33 +0100

    Added unique index to username in userschema

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,7 @@ module.exports = function(mongoose)
 {
     var UserSchema = new mongoose.Schema(
         {
-            username    :  {type:String,required:true},
+            username    :  {type:String,required:true,unique: true},
             password    :  {type:String,required:true,bcrypt: true},
             email       :  {type:String,required:true},
             firstName   :  {type:String,required:true},
