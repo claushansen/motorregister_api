329520e97cf73723679f33bfecf253e1861beff8
Morten Halling <mortenh@gmx.com>
2016-04-21 12:05:05 +0200

    Clean up in footer and home. Created help view and controller

--- public/partials/help/help.controller.js
+++ public/partials/help/help.controller.js
@@ -1,8 +1,23 @@
 (function(){
     angular
-        .module("BILAPI")
-        .controller("HelpController",HelpController);
+        .module("BILAPI").controller("HelpController",HelpController);
     function HelpController($scope){
         $scope.hello = "hi from HelpController";
+
+        $scope.oneAtATime = true;
+
+        $scope.groups = [
+            {
+                title: 'Hvad er BilApi?',
+                content: 'BilApi er en tjeneste der udbydes til virksomheder i form af værksteder, undervognsbehandlere og andre der servicerer køretøjer. ' +
+                'Ved at benytte det brugervenlige grænseflade kan disse virksomheder opsætte og tilpasse en prisberegner. ' +
+                'Privatpersoner kan ved hjælp af deres registreringsnummer få tilbudt en pris for en ydelse ved virksomheden og vælge at kontakte virksomheden.'
+            }
+        ];
+
+        $scope.status = {
+            isFirstOpen: true,
+            isFirstDisabled: false
+        };
     }
 })();
